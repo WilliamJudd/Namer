a89e121714b738c398c79a03c8cfe3e7fe86c1e2
kingjudd <wjiv33@gmail.com>
2015-04-17 18:43:52 -0400

    Custom Cell
    Added blur view to some pictures in a table view cell

---
+++ NamesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NamesViewController.swift
+//  Namer
+//
+//  Created by William Judd on 4/17/15.
+//  Copyright (c) 2015 William Judd. All rights reserved.
+//
+
+import UIKit
+
+class NamesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
